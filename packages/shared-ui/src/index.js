3509e2c06d25467767c4fb91a4b0e473ccdc1d4e
chapmancheng <chapboy1989@gmail.com>
2021-07-13 09:31:09 +0800

    sucessfully added monorepo dependency

---
+++ packages/shared-ui/src/index.js
@@ -0,0 +1,8 @@
+import Button from './components/Button'
+import Header from './components/Header'
+
+const obj =  {
+    Button, Header
+}
+
+export default obj
---
+++ packages/shared-ui/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+module.exports = {
+    mode: "development",
+
+    entry: "./src/index.js",
+
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist"),
+        library: {
+            // ! you don't need a name
+            // name: "laundryui",
+            type: "umd",
+            export: "default",
+            auxiliaryComment: "this is a auxiliary comment",
+            umdNamedDefine: true            
+        },
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+        ]
+    },
+    plugins: [],
+
+    externals: [nodeExternals()]
+}
