3f69f8666f3362da9dfa3862a6061626aba6ee21
chapmancheng <chapboy1989@gmail.com>
2021-07-29 11:17:47 +0800

    mv src/index to include subModules

---
+++ packages/shared-ui/src/components/index.js
@@ -0,0 +1,12 @@
+import Button from "./Button";
+import Header from "./Header";
+import ButtonJSX from "./ButtonJSX";
+import ButtonTS from "./ButtonTS";
+
+export default  {
+	Button,
+	Header,
+	ButtonJSX,
+	ButtonTS,
+};
+
--- packages/shared-ui/src/index.js
+++ packages/shared-ui/src/index.js
@@ -1,12 +1,1 @@
-import Button from "./components/Button";
-import Header from "./components/Header";
-import ButtonJSX from "./components/ButtonJSX";
-import ButtonTS from "./components/ButtonTS";
-
-export default  {
-	Button,
-	Header,
-	ButtonJSX,
-	ButtonTS,
-};
-
+export { default } from './components'
